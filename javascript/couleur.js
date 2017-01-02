21b50398453381fbbd7adf5f3ca40e558edf903d
projetinfo12 <prenom2a@gmail.com>
2017-01-02 16:32:38 +0100

    Add files via upload

---
+++ javascript/couleur.js
@@ -0,0 +1,5 @@
+function c() {
+	
+	var couleur=document.getElementById('couleur').value;
+	document.body.style.backgroundColor = couleur;
+}
---
+++ javascript/nom.js
@@ -0,0 +1,3 @@
+var Nom=prompt("Entrez votre Nom");
+var Prenom=prompt("Entrez votre Prenom");
+document.getElementById("nom").innerHTML+='Bienvenue '+Nom+' '+Prenom+' ';
